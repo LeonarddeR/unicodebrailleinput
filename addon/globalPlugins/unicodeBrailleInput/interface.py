bb7a6175a7b1c8488f683bdd642ffe947eb86f67
Leonard de Ruijter <alderuijter@gmail.com>
2026-05-06 20:58:16 +0200

    Cleanup

--- addon/globalPlugins/unicodeBrailleInput/interface.py
+++ addon/globalPlugins/unicodeBrailleInput/interface.py
@@ -38,11 +38,11 @@ class InputType(DisplayStringIntEnum):
 			self.DOTS: _("Braille dots (e.g. 1345-1236-145-1)"),
 			# Translators: the label of an input type
 			self.TEXT_OUTPUT_TABLE: _("Normal text according to {table}").format(
-				table=braille.handler.table.displayName
+				table=braille.handler.table.displayName,
 			),
 			# Translators: the label of an input type
 			self.TEXT_INPUT_TABLE: _("Normal text according to {table}").format(
-				table=brailleInput.handler.table.displayName
+				table=brailleInput.handler.table.displayName,
 			),
 		}
 
@@ -160,16 +160,18 @@ def dotsToUnicode(
 	@param regularSpace: if True, space will be replaced by a regular one instead of the braille space
 	@return: the result in Unicode (NVDA in our example)
 	"""
-	cells = cells.translate({
-		ord("f"): "1",
-		ord("d"): "2",
-		ord("s"): "3",
-		ord("j"): "4",
-		ord("k"): "5",
-		ord("l"): "6",
-		ord("a"): "7",
-		ord(";"): "8",
-	}).strip()
+	cells = cells.translate(
+		{
+			ord("f"): "1",
+			ord("d"): "2",
+			ord("s"): "3",
+			ord("j"): "4",
+			ord("k"): "5",
+			ord("l"): "6",
+			ord("a"): "7",
+			ord(";"): "8",
+		},
+	).strip()
 	invalidStrings = invalidInputRegexp.findall(cells)
 	# Translators: Error message displayed when the user enters invalid input.
 	msg = _("Unexpected input: '%s', only dots 0 to 8 and - are allowed.") % "', '".join(invalidStrings)
